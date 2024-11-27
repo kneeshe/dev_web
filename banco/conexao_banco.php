55a950c82012bbd00b72b0141bb992e411121750
Cris Sousa <cristian_sousa@live.com>
2024-11-26 21:48:42 -0300

    Add files via upload

--- banco/conexao_banco.php
+++ banco/conexao_banco.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "dev_web_db";
+$dbname = "FilmeDB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
