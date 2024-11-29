5f342085c6c35b987667d3e7c062db1a48baf163
Cris Sousa <cristian_sousa@live.com>
2024-11-29 16:56:56 -0300

    Add files via upload

--- paginas/inscreva.php
+++ paginas/inscreva.php
@@ -14,49 +14,65 @@
         <h1 id="flip">O Ocultista Cinéfilo</h1>
         
         <ul>
-        <li><a href="index.php" id="inicio"> Inicio </a></li>
-        <li><a href="sobre.php" id="sobre"> Sobre </a></li>
-	    <li><a href="creditos.php" id="creditos">Créditos</a></li> 
-	    <li><a href="analises.php" id="analises"> Análises </a></li>
-        <li><a href="noticias.php" id="noticias"> Notícias </a></li>
-        <li style="float:right"><a href="inscreva.php" id="inscreva" style="background-color: #12151c"> Inscreva-se </a></li>
-        <li style="float:right"><a href="login.php" id="login"> Login </a></li>
+            <li><a href="index.php" id="inicio"> Inicio </a></li>
+            <li><a href="sobre.php" id="sobre"> Sobre </a></li>
+            <li><a href="creditos.php" id="creditos">Créditos</a></li> 
+            <li><a href="analises.php" id="analises"> Análises </a></li>
+            <li><a href="noticias.php" id="noticias"> Notícias </a></li>
+            <li style="float:right"><a href="inscreva.php" id="inscreva" style="background-color: #12151c"> Inscreva-se </a></li>
+            <li style="float:right"><a href="login.php" id="login"> Login </a></li>
         </ul>
 
         <img id="gato" src="../imagens/demo_gato.jpg" alt="gato do demo">
         <h2 style="color:white">Quer receber noticias, informações e atualizações? Se inscreva na nossa Newsletter Vermelha: </h2>
 
 	<div id="inscreva" class="inscreva">
-          <form id="form_inscricao" class="form_inscricao" name="form_inscricao"method="POST" action="../banco/conexao.php" onsubmit="valida_entrada()"> <!-- formulario para validar os dados e envia-los para o banco de dados -->
-		
-		
-		    <p><div id="insc_nome">Nome:<input type="text" id="nome" name="nome" placeholder="Aqui você coloca seu nome :)"></div> <!-- entrada de nome inseria pelo usuario -->
+        <form id="form_inscricao" class="form_inscricao" name="form_inscricao"method="POST" action="../banco/conexao.php" onsubmit="valida_entrada()"> <!-- formulario para validar os dados e envia-los para o banco de dados -->
+            
+            
+            <p> 
+                <div id="insc_nome">
+                    Nome:<input type="text" id="nome" name="nome" placeholder="Aqui você coloca seu nome :)">
+                </div> <!-- entrada de nome inseria pelo usuario -->
                 <span id="erro_nome" class="mensagem_erro"></span> <!-- essa tag fica invisivel ate que a função de validação a chame, caso ocorra algum erro no input do usuario -->
             </p>
-	   
-		    <p><div id="insc_email">Email: <input type="text" id="email" name="email" placeholder="Digite seu email aqui :)"></div> <!-- mesmo caso citado acima, porem para emails -->
+        
+            <p>
+                <div id="insc_email">
+                    Email: <input type="text" id="email" name="email" placeholder="Digite seu email aqui :)">
+                </div> <!-- mesmo caso citado acima, porem para emails -->
                 <span id="erro_email" class="mensagem_erro"></span>
-	        </p>
-		
-            <p><div id="insc_genero">És boy?<input type="radio" id="genero" name="genero" value="menino"> <!-- cria um mini menu para o usuario escolher a qual genero se identifica -->
-               ou És boya?<input type="radio" id="genero" name="genero" value="menina"></div>
-              <span id="erro_genero" class="mensagem_erro"></span>
+            </p>
+            
+            <p>
+                <div id="insc_genero">És boy?<input type="radio" id="genero" name="genero" value="menino"> <!-- cria um mini menu para o usuario escolher a qual genero se identifica -->
+                ou És boya?<input type="radio" id="genero" name="genero" value="menina">
+                </div>
+                <span id="erro_genero" class="mensagem_erro"></span>
             </p>
 
-            <p><div id="insc_text">Fale um pouco sobre você:</p><!-- caixa de texto para o usuario falar de si -->
-            	<p><textarea rows="5" cols="50" id="comentario" name="comentario"></textarea></div>
+            <p>
+                <div id="insc_text">Fale um pouco sobre você:</p><!-- caixa de texto para o usuario falar de si -->
+                    <p><textarea rows="5" cols="50" id="comentario" name="comentario"></textarea>
+                </div>
                 <span id="erro_comentario" class="mensagem_erro"></span>
-	        </p>
+            </p>
             <br>
 
-		<h3>Deseja receber informações e ofertas de nossos patrocinadores?</h3><!-- pergunta se o usuario deseja receber noticias dos patrocinadores do site -->
-            <td><p><input type="radio" id="oferta" name="oferta" value="sim"> Sim, quero <!-- ele pode aceitar ou recusar -->
-              <br>
-            <input type="radio" id="oferta" name="oferta" value="nao"> Não, não quero
-              <span id="erro_oferta" class="mensagem_erro"></span>
-	    </p>
-		<div id="insc_botao"><p><input type="submit" value="Enviar" id="submit" name="submit"></p></div><!-- botão que envia os dados do input para a função do js e o script do php -->
-	</form>
+            <h3>Deseja receber informações e ofertas de nossos patrocinadores?</h3><!-- pergunta se o usuario deseja receber noticias dos patrocinadores do site -->
+                <td>
+                    <p><input type="radio" id="oferta" name="oferta" value="sim"> Sim, quero <!-- ele pode aceitar ou recusar -->
+            <br>
+                        <input type="radio" id="oferta" name="oferta" value="nao"> Não, não quero
+                        <span id="erro_oferta" class="mensagem_erro"></span>
+                    </p>
+		    
+            <div id="insc_botao">
+                <p>
+                    <input type="submit" value="Enviar" id="submit" name="submit">
+                </p>
+            </div><!-- botão que envia os dados do input para a função do js e o script do php -->
+	    </form>
 	</div>
 
     <footer> . </footer>
--- paginas/noticias.php
+++ paginas/noticias.php
@@ -14,44 +14,44 @@
         <h1 id="flip">O Ocultista Cinéfilo</h1>
         
         <ul>
-        <li><a href="index.php" id="inicio"> Inicio </a></li>
-        <li><a href="sobre.php" id="sobre"> Sobre </a></li>
-	      <li><a href="creditos.php" id="creditos">Créditos</a></li> 
-	      <li><a href="analises.php" id="analises"> Análises </a></li>
-        <li><a href="noticias.php" id="noticias" style="background-color: #12151c"> Notícias </a></li>
-        <li style="float:right"><a href="inscreva.php" id="inscreva"> Inscreva-se </a></li>
-        <li style="float:right"><a href="login.php" id="login"> Login </a></li>
+          <li><a href="index.php" id="inicio"> Inicio </a></li>
+          <li><a href="sobre.php" id="sobre"> Sobre </a></li>
+          <li><a href="creditos.php" id="creditos">Créditos</a></li> 
+          <li><a href="analises.php" id="analises"> Análises </a></li>
+          <li><a href="noticias.php" id="noticias" style="background-color: #12151c"> Notícias </a></li>
+          <li style="float:right"><a href="inscreva.php" id="inscreva"> Inscreva-se </a></li>
+          <li style="float:right"><a href="login.php" id="login"> Login </a></li>
         </ul>
 
         <img id="gato" src="../imagens/demo_gato.jpg" alt="gato do demo">
 
         <div class="div_noticia"> <!--cria a div onde vai o bloco das noticias -->
-          <p>
-          <h2>Diogo é visto sendo gado em Ribeirão Preto</h2> <!-- chamada da materia -->
-            Como não é surpresa para ninguém, nosso querido Diogo, mais conhecido como Didi, foi visto sendo gado de uma garota.
+            <p>
+              <h2>Diogo é visto sendo gado em Ribeirão Preto</h2> <!-- chamada da materia -->
+                Como não é surpresa para ninguém, nosso querido Diogo, mais conhecido como Didi, foi visto sendo gado de uma garota.
+                <br><br>
+                Boatos dizem que era a terceira garota da semana. Testemunhas afirmam que era uma menina gótica, com problemas paternos.
+                <br><br>
+                Nossos repórteres já estão no encalço do Diogo para esclarecimento, porém ele se nega a dar um relato. Podem ter certeza, 
+                caros leitores, não desistiremos da verdade.
+            </p>
             <br><br>
-            Boatos dizem que era a terceira garota da semana. Testemunhas afirmam que era uma menina gótica, com problemas paternos.
+
+            <p>
+              <h2>Estudos afirmam que trabalhar até tarde causa problemas de saúde</h2>
+
+              Estudos feitos por mim mesmo, que está trabalhando neste site faz algumas horas, e já são 3 da manhã,
+              afirmam que a falta de descanso, podem acarretar em severas perdas de noção e bom senso. 
+              <br><br>
+              Será que chego vivo até as 4 da manhã?
+            </p>
             <br><br>
-            Nossos repórteres já estão no encalço do Diogo para esclarecimento, porém ele se nega a dar um relato. Podem ter certeza, 
-            caros leitores, não desistiremos da verdade.
-          </p>
-          <br><br>
 
-          <p>
-          <h2>Estudos afirmam que trabalhar até tarde causa problemas de saúde</h2>
+            <p>
+              <h2>Sensitiva afirma que este trabalho irá ganhar a nota máxima!</h2>
 
-          Estudos feitos por mim mesmo, que está trabalhando neste site faz algumas horas, e já são 3 da manhã,
-          afirmam que a falta de descanso, podem acarretar em severas perdas de noção e bom senso. 
-          <br><br>
-          Será que chego vivo até as 4 da manhã?
-          </p>
-          <br><br>
-
-          <p>
-            <h2>Sensitiva afirma que este trabalho irá ganhar a nota máxima!</h2>
-
-            Nossa equipe jornalistica conversou com a Samuel(a) sensitiva que afirmou que este projeto receberá a nota máxima pelo Professor.
-          </p>
+              Nossa equipe jornalistica conversou com a Samuel(a) sensitiva que afirmou que este projeto receberá a nota máxima pelo Professor.
+            </p>
 
         </div>
 
