c9048029aae5812d44feeb13177deee11463edde
Cris Sousa <cristian_sousa@live.com>
2024-11-28 22:02:32 -0300

    Add files via upload

---
+++ paginas/under.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Site em construção</title>
+	</head>
+
+	<body style="background-image:url('../imagens/pikachu.gif'); background-repeat:no-repeat; background-position: 50% 50% 50% 50%">
+		<a href="../paginas/index.php">Clique aqui para entrar no inferno</a>
+	</body>
+
+</html>
